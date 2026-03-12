965328fa678b7f4ce028dff36ed7a15df0bdab44
VladaKov <vlada.Kovalenko.02@mail.ru>
2026-03-12 12:54:49 +0300

    The first commit of the branch

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     const router = useRouter();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff'}}>
       <Text style={{ fontSize: 24, marginBottom: 20 }}>👋 Привет, мир!</Text>
     </View>
   );
