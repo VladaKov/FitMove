af2c580aac1ed3ec24ce4ffe17308070efd6e72e
VladaKov <vlada.Kovalenko.02@mail.ru>
2026-03-25 16:29:31 +0300

    small update main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
-from routers import routeUsers as users, routeClients as clients
+from routers import routeUsers, routeClients, routeWorkout, routeExercises, routeBlockExercises, routeCategory
 from model import init_db
 
 app = FastAPI()
 
-app.include_router(users.router)
-app.include_router(clients.router)
+app.include_router(routeUsers.router)
+app.include_router(routeClients.router)
+app.include_router(routeWorkout.router)
+app.include_router(routeExercises.router)
+app.include_router(routeBlockExercises.router)
+app.include_router(routeCategory.router)
 
 init_db()
 
